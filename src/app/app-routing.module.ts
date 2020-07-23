e8a68fd7f2e3ad3a6ad63a9778c3ecbe5cd70198
claudemir ramos ferreira <claudemir@cal-comp.com.br>
2020-07-23 19:37:32 -0400

    corrigir o bug na rota entregador

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {path : 'list-produto', component : ListProdutoComponent},
-  {path : 'list-entegador', component : ListEntregadorComponent}
+  {path : 'list-entregador', component : ListEntregadorComponent}
 ];
 
 @NgModule({
