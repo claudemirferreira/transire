1bafb677a4d2c459d28448b90b9544f2b24a4a92
claudemir <claudemirramosferreira@gmail.com>
2020-08-20 05:46:21 -0400

    add auto complete

--- src/app/pedido/new-pedido/new-pedido.component.ts
+++ src/app/pedido/new-pedido/new-pedido.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProdutoService } from 'src/app/produto/produto.service';
 import { FormControl, FormBuilder } from '@angular/forms';
-import { startWith, map } from 'rxjs/operators';
+import { startWith, map, takeUntil, take } from 'rxjs/operators';
 import { Produto } from 'src/app/produto/produto';
 
 import { ResponseApi } from '../../shared/response-api';
+import { ReplaySubject, Subject } from 'rxjs';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-new-pedido',
@@ -18,17 +20,91 @@ export class NewPedidoComponent implements OnInit {
 
   post: any = '';
 
-  list : Produto[];
   stateCtrl : Produto;
   filterProduto: Produto = new Produto();
 
-  formGroup = new FormControl();
+  /** list of banks */
+  protected list: Produto[] ;
+
+  /** control for the selected bank */
+  public bankCtrl: FormControl = new FormControl();
+
+  /** control for the MatSelect filter keyword */
+  public bankFilterCtrl: FormControl = new FormControl();
+
+  /** list of banks filtered by search keyword */
+  public filteredBanks: ReplaySubject<Produto[]> = new ReplaySubject<Produto[]>(1);
+
+  @ViewChild('singleSelect', { static: true }) singleSelect: MatSelect;
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
+
 
   constructor(private produtoService : ProdutoService,
-    private formBuilder: FormBuilder) { }
+    private formBuilder: FormBuilder) {
+      this.pesquisar();
+    }
+
+  init(){
+    // set initial selection
+    this.bankCtrl.setValue(this.list[6]);
+
+    // load the initial bank list
+    this.filteredBanks.next(this.list.slice());
+
+    // listen for search field value changes
+    this.bankFilterCtrl.valueChanges
+      .pipe(takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.filterBanks();
+      });
+  }
 
   ngOnInit() {
-    this.pesquisar();
+  }
+
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  /**
+   * Sets the initial value after the filteredBanks are loaded initially
+   */
+  protected setInitialValue() {
+    this.filteredBanks
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
+        // the form control (i.e. _initializeSelection())
+        // this needs to be done after the filteredBanks are loaded initially
+        // and after the mat-option elements are available
+        this.singleSelect.compareWith = (a: Produto, b: Produto) => a && b && a.id === b.id;
+      });
+  }
+
+  protected filterBanks() {
+    if (!this.list) {
+      return;
+    }
+    // get the search keyword
+    let search = this.bankFilterCtrl.value;
+    if (!search) {
+      this.filteredBanks.next(this.list.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredBanks.next(
+      this.list.filter(bank => bank.nome.toLowerCase().indexOf(search) > -1)
+    );
   }
 
   pesquisar() {
@@ -36,37 +112,10 @@ export class NewPedidoComponent implements OnInit {
     this.produtoService.listarTodos().subscribe((responseApi: ResponseApi) => {
       this.list = responseApi['content'];
       console.log(JSON.stringify(this.list));
+      this.init();
       //this.ngxLoader.stop();
     });
   }
 
-  getErrorEmail() {
-    return this.formGroup.get('quantidade').hasError('required') ? 'Field is required' :
-        this.formGroup.get('quantidade').hasError('alreadyInUse') ? 'This emailaddress is already in use' : '';
-  }
-
-  selectedclient(event) {
-    console.log(event.option.value);
-  }
-
-  selectEvent(item) {
-    // do something with selected item
-    console.log('selectEvent');
-  }
-
-  onChangeSearch(search: string) {
-    // fetch remote data from here
-    // And reassign the 'data' which is binded to 'data' property.
-    console.log('onChangeSearch');
-  }
-
-  onFocused(e) {
-    // do something
-    console.log('onChangeSearch');
-  }
-
-  onSearchChange($event) {
-    $event.stopPropagation();
-  }
 
 }
